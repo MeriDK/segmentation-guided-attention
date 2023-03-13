ecfa6580918147aebf7366406e2ddf1709897ce4
MeriDK <ma.rizhko@gmail.com>
2023-03-13 09:57:29 -0400

    Fixed more seeds, moved dataset creation to separate function

--- main.py
+++ main.py
@@ -6,52 +6,55 @@ from torch.nn import BCEWithLogitsLoss
 from torch.utils.data import DataLoader
 from torch.optim.lr_scheduler import ExponentialLR
 
-from utils import build_model, get_transformations, setup_criterion
+from utils import build_model, get_transformations, setup_criterion, setup_dataloader
 from dataset import KidneyDataset
 from trainer import Trainer
 from loss import AttentionLoss
+import random
+import numpy as np
+import os
+
+
+# set up random seed for reproducibility
+torch.manual_seed(42)
+random.seed(42)
+np.random.seed(42)
+os.environ['CUBLAS_WORKSPACE_CONFIG'] = ':4096:8'
+torch.use_deterministic_algorithms(True)
 
 
 def run_experiment():
-
-    # set up random seed for reproducibility
-    torch.manual_seed(42)
 
     # set up augmentation
     data_transforms = get_transformations()
 
     # all changeable parameters should be specified here
     wandb.config.update({
-        'batch_size': 128,
+        'batch_size': 32,
         'num_epochs': 30,
         'threshold': 0.5,
         'loss': 'BCEAttention',     # 'BCE' 'AttentionLoss' 'BCEAttention'
-        'model_name': 'resnet18',   # 'swin'/'vit' + '_'  + 'small', 'tiny', 'base' || 'resnet' + '18'/'50'
-        'lr': 0.001,
-        'weight_decay': 0.01,
-        'gamma': 1,
+        'model_name': 'resnet50',   # 'swin'/'vit' + '_'  + 'small', 'tiny', 'base' || 'resnet' + '18'/'50'
+        'lr': 0.01,
+        'weight_decay': 0.001,
+        'gamma': 0.85,
         'freeze': False,
         'freeze_until': 'layer3',
         'device': 'cuda',
         'pretrained': True,
         'data_transforms': data_transforms,
         'data_path': '/home/mrizhko/hn_miccai/data/',
-        'data_type': '_seg_only'    # '' / '_seg' / 'seg_only'
+        'data_type': '_seg_only',    # '_seg' / '_seg_only' / '_seg_n_seg_only'
+        'save_weights': True,
+        'weights_path': ''  # '' / '/home/mrizhko/hn_miccai/weights/model.pth'
     })
 
     # setup model
     model = build_model(wandb.config)
     model = model.to(wandb.config['device'])
 
-    # create train and valid datasets
-    train_dataset = KidneyDataset(f'SickKids_train{wandb.config["data_type"]}.csv', wandb.config['data_path'],
-                                  data_transforms['train'])
-    valid_dataset = KidneyDataset(f'SickKids_valid{wandb.config["data_type"]}.csv', wandb.config['data_path'],
-                                  data_transforms['valid'])
-
-    # pass datasets to pytorch loaders
-    train_loader = DataLoader(train_dataset, batch_size=wandb.config['batch_size'], shuffle=True)
-    valid_loader = DataLoader(valid_dataset, batch_size=wandb.config['batch_size'])
+    # create train dataset and dataloaders
+    train_loader, valid_loader = setup_dataloader(wandb.config, data_transforms)
 
     # set up loss function
     criterion = setup_criterion(wandb.config, model)
@@ -70,13 +73,14 @@ def run_experiment():
 if __name__ == '__main__':
 
     # init wandb
-    wandb.init(project="test", entity="meridk")
+    wandb.init(project="hn_miccai_final_eval", entity="meridk")
 
     # run experiment
     run_experiment()
 
     # save the model to wandb
-    wandb.save(os.path.join(wandb.run.dir, 'model.pth'))
+    if wandb.config['save_weights']:
+        wandb.save(os.path.join(wandb.run.dir, 'model.pth'))
 
     # explicitly end wandb
     wandb.finish()
