2d425aa9ca920075ce95662f7e5f6f75a4d2ec96
MeriDK <ma.rizhko@gmail.com>
2023-03-13 09:59:30 -0400

    Added attention score function, dataset creation function, switched loading weights from local files to wandb

--- utils.py
+++ utils.py
@@ -4,6 +4,13 @@ import albumentations.pytorch
 from gradcam import GCAM
 from torch.nn import BCEWithLogitsLoss
 from loss import AttentionLoss
+from torch.utils.data import DataLoader
+from dataset import KidneyDataset
+import torch
+import random
+import sys
+import numpy as np
+import wandb
 
 
 def build_model(config):
@@ -19,6 +26,11 @@ def build_model(config):
 
     # create model
     model = timm.create_model(model_name, pretrained=config['pretrained'], num_classes=1)
+
+    # load weights
+    if config['weights_path']:
+        model_path = wandb.restore('model.pth', run_path=config['weights_path'])
+        model.load_state_dict(torch.load(model_path.name))
 
     # freeze model
     if config['freeze']:
@@ -64,7 +76,7 @@ def create_cam(model, model_name):
         target_layer = model.layer4[-1]
         cam = GCAM(model, target_layer, use_cuda=True)
     else:
-        target_layer = [model.blocks[-1].norm1]
+        target_layer = model.blocks[-1].norm1
         cam = GCAM(model, target_layer, use_cuda=True, reshape_transform=reshape_transform)
 
     return cam
@@ -87,3 +99,52 @@ def setup_criterion(config, model):
         raise NotImplementedError(f'Unknown loss')
 
     return criterion
+
+
+def setup_dataloader(config, data_transforms):
+
+    # create train dataset and dataloaders
+    if config['data_type'] in ('_seg', '_seg_only'):
+        # create train and valid datasets
+        train_dataset = KidneyDataset(f'SickKids_train{config["data_type"]}.csv', config['data_path'],
+                                      data_transforms['train'])
+        valid_dataset = KidneyDataset(f'SickKids_valid{config["data_type"]}.csv', config['data_path'],
+                                      data_transforms['valid'])
+        # pass datasets to pytorch loaders
+        train_loader = DataLoader(train_dataset, batch_size=config['batch_size'], shuffle=True)
+        valid_loader = DataLoader(valid_dataset, batch_size=config['batch_size'])
+
+    else:  # seg_n_seg_only
+        # the first dataset is the full dataset
+        train_dataset1 = KidneyDataset(f'SickKids_train_seg.csv', config['data_path'],
+                                       data_transforms['train'])
+        # the second dataset consists only of images with masks
+        train_dataset2 = KidneyDataset(f'SickKids_train_seg_only.csv', config['data_path'],
+                                       data_transforms['train'])
+        # pass datasets to pytorch loaders
+        train_loader1 = DataLoader(train_dataset1, batch_size=config['batch_size'], shuffle=True)
+        train_loader2 = DataLoader(train_dataset2, batch_size=config['batch_size'], shuffle=True)
+        train_loader = [train_loader1, train_loader2]
+
+        # create valid dataset and dataloaders
+        valid_dataset = KidneyDataset(f'SickKids_valid_seg.csv', config['data_path'],
+                                      data_transforms['valid'])
+        valid_loader = DataLoader(valid_dataset, batch_size=config['batch_size'])
+
+    return train_loader, valid_loader
+
+
+def attention_accuracy(gcam, mask):
+
+    resp = (gcam * mask).sum()
+    resn = (gcam * (1 - mask)).sum()
+
+    if resp == 0 and resn == 0:
+        attn_acc = torch.tensor(0)
+    else:
+        attn_acc1 = resp / (resp + resn)
+        attn_acc2 = resp / mask.sum()
+
+        attn_acc = (attn_acc1 + attn_acc2) / 2
+
+    return attn_acc.item()
